fd24d0d1391671ccf609ea5c16988139b9063846
azeem147 <azm2006@gmail.com>
2020-06-14 18:43:18 +0500

    Starting Automating Boring Stuff With Python on this branch

---
+++ Automating Boring Stuff With Python/Section 2/Flow Control.py
@@ -0,0 +1,18 @@
+# Expressoin always reduce down to a single value
+print(2 + 2)
+# Variables
+spam = 'Hello '
+print(spam * 3)
+# if it reduces down to a single value it's an expression, otherwise it's
+# a statement
+
+# First Program hello.py
+print('Hello World')
+print("What's your name?")  # Ask for name
+myName = input('Azeem')
+print("Nice to meet you, " + myName)
+print('The length of your name is:')
+print(len(myName))
+print("What's your age?")
+myAge = input(23)
+print("You will be" + str(int(myAge) + 1) + 'in a year')
