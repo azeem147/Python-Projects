0c7744d1a68bcd3b72c72e64dcf62c6c47cb5891
azeem147 <azm2006@gmail.com>
2020-06-18 23:14:46 +0500

    Sec 3 is finished!

---
+++ Automating Boring Stuff With Python/Sec_3/global_local_scope.py
@@ -0,0 +1,13 @@
+spam = 42  # gloabl scope
+
+
+def eggs():
+<<<<<<< HEAD
+    spam = 42  # local scope. Also local variabls can't be used in global scope
+=======
+    spam = 42  # local scope
+>>>>>>> e77c042fe46d368d76fb89195695c5c0811d2268
+
+
+print('Print some code!')
+print('Print some code!')
