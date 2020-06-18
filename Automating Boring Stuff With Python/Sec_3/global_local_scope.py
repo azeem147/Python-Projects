688b2efe2b8cce8eccd534f2178a8a2473fb63d0
azeem147 <azm2006@gmail.com>
2020-06-18 23:16:34 +0500

    Please ignore previous branch 3! This is the latest!

--- Automating Boring Stuff With Python/Sec_3/global_local_scope.py
+++ Automating Boring Stuff With Python/Sec_3/global_local_scope.py
@@ -2,15 +2,7 @@ spam = 42  # gloabl scope
 
 
 def eggs():
-<<<<<<< HEAD
-<<<<<<< HEAD
     spam = 42  # local scope. Also local variabls can't be used in global scope
-=======
-    spam = 42  # local scope
->>>>>>> e77c042fe46d368d76fb89195695c5c0811d2268
-=======
-    spam = 42  # local scope
->>>>>>> e77c042fe46d368d76fb89195695c5c0811d2268
 
 
 print('Print some code!')
