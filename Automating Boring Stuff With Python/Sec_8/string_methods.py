0d2537186d5a3e63bca876bca9464b52b74f6aaa
azeem147 <azm2006@gmail.com>
2020-07-07 18:11:07 +0500

    Sec 8 is finished!

---
+++ Automating Boring Stuff With Python/Sec_8/string_methods.py
@@ -0,0 +1,11 @@
+spam = 'Hello World'
+print(spam.upper())
+print(spam.lower())
+
+# String Formatting
+name = 'Azeem'
+place = 'PWD'
+time = '6 pm'
+food = 'meat'
+
+print('Hello! %s, you are invited to a part at %s at %s. Please bring %s' % (name, place, time, food))
---
+++ Automating Boring Stuff With Python/Sec_8/strings_syntax.py
@@ -0,0 +1,5 @@
+# how to use excape characters
+string = 'Say hi to Bob\'s mother'
+
+print(string)
+print('Hello! How are you?\nI\'m fine!\nWhat about you?')
