4d950429f85a3a62e51dc3afdef8768e0f5adc72
azeem147 <azm2006@gmail.com>
2020-10-16 18:32:26 +0500

    Milestone project 1 for Python Bootcamp

---
+++ Complete Python 3 Bootcamp/Milestone Project-1-Azeem/tictactoe.py
@@ -0,0 +1,15 @@
+# TO DO:
+
+# Present the game board so user can select what to choose
+
+# Accept the User Input within constraints
+
+# Save the user input and show the update board
+
+# Once the craiteria is met, declare the player winner.
+
+# Give option to restart the game
+
+# Warm-up exercicse: https://colab.research.google.com/drive/1x3xQ41dOoexQimQl27si8VdsnWx90WVg
+
+# Walkthrough Steps: https://colab.research.google.com/drive/1UNSoLWcIQUx_ucOVbsRrrW3hdCR7dgTe#scrollTo=uF4E1PB7bdt3
